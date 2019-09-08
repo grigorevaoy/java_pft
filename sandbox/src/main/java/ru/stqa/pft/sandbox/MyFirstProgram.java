badb226b1604f3ccda6d0999bbc8f08de9c809f8
grigorevaoy <55024506+grigorevaoy@users.noreply.github.com>
2019-09-08 17:06:59 +0300

    отреформатирован стиль кода

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,8 +1,8 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
-    
-    public static void main(String[] args) { 
-        System.out.println("This is my first program"); 
-    } 
+
+  public static void main(String[] args) {
+    System.out.println("This is my first program");
+  }
 } 
