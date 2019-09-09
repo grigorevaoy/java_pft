34a928270ba05335f1e737b04a829fd3515a0f18
grigorevaoy <55024506+grigorevaoy@users.noreply.github.com>
2019-09-09 23:10:57 +0300

    выполнено третье задание

---
+++ secondtask/src/test/java/ru/stqa/pft/secondtask/PointTest.java
@@ -0,0 +1,13 @@
+package ru.stqa.pft.secondtask;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTest {
+  @Test
+  public void testArea(){
+    Point p1 = new Point(5, 5);
+    Point p2 = new Point(4, 4);
+    Assert.assertEquals(Point.distance(p1,p2),1.4142135623730951);
+  }
+}
