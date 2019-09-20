5873988b7e1b79807f99d009ce0518e1f225a9cc
grigorevaoy <55024506+grigorevaoy@users.noreply.github.com>
2019-09-21 01:06:52 +0300

    оптимизированы переходы между страницами

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -4,11 +4,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class NavigationHelper extends HelperBase{
-  private FirefoxDriver wd;
+public class NavigationHelper extends HelperBase {
 
   public NavigationHelper(WebDriver wd) {
-   super(wd);
+    super(wd);
   }
 
   public void returnToHomePage() {
@@ -16,11 +15,19 @@ public class NavigationHelper extends HelperBase{
   }
 
   public void gotoGroupPage() {
-  click(By.linkText("groups"));
+    if (isElementPresent(By.tagName("h1"))
+            && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+            && isElementPresent(By.name("new"))) {
+      return;
+    }
+    click(By.linkText("groups"));
   }
 
 
   public void gotoContactPage() {
+    if (isElementPresent(By.id("maintable"))){
+      return;
+    }
     click(By.linkText("home"));
   }
 }
