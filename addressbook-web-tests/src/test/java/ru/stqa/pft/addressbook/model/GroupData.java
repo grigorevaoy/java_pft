723dfaf85cfd5d4e28d091968810c9dac686dbde
grigorevaoy <55024506+grigorevaoy@users.noreply.github.com>
2019-09-28 00:38:55 +0300

    лекция 4.8

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -3,27 +3,20 @@ package ru.stqa.pft.addressbook.model;
 import java.util.Objects;
 
 public class GroupData {
-  private final String id;
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  private int id;
   private final String name;
   private final String header;
   private final String footer;
 
-  public GroupData(String id, String name, String header, String footer) {
+  public GroupData(int id, String name, String header, String footer) {
     this.id = id;
     this.name = name;
     this.header = header;
     this.footer = footer;
-  }
-
-  public GroupData(String name, String header, String footer) {
-    this.id = null;
-    this.name = name;
-    this.header = header;
-    this.footer = footer;
-  }
-
-  public String getId() {
-    return id;
   }
 
   @Override
@@ -31,13 +24,24 @@ public class GroupData {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
     GroupData groupData = (GroupData) o;
-    return Objects.equals(id, groupData.id) &&
+    return id == groupData.id &&
             Objects.equals(name, groupData.name);
   }
 
   @Override
   public int hashCode() {
     return Objects.hash(id, name);
+  }
+
+  public GroupData(String name, String header, String footer) {
+    this.id = 0;
+    this.name = name;
+    this.header = header;
+    this.footer = footer;
+  }
+
+  public int getId() {
+    return id;
   }
 
   @Override
