9aeb7b9fb63b89a1701253bb57beaf9be974d0fe
grigorevaoy <55024506+grigorevaoy@users.noreply.github.com>
2019-09-27 20:53:26 +0300

    лекция 4.1

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PrimeTests.java
@@ -0,0 +1,24 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PrimeTests {
+
+  @Test
+  public void testPrimes(){
+    Assert.assertTrue(Primes.isPrime(Integer.MAX_VALUE));
+  }
+
+  @Test
+  public void testoNonPrimes(){
+    Assert.assertFalse(Primes.isPrime(Integer.MAX_VALUE - 2));
+  }
+
+  @Test (enabled = false)
+  public void testPrimesLong(){
+    long n = Integer.MAX_VALUE;
+    Assert.assertTrue(Primes.isPrime(n));
+  }
+
+}
