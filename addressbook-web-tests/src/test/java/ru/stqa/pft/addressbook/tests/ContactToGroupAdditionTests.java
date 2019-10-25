36804d60600e028f5b66c82b06f3024987ca8c39
grigorevaoy <55024506+grigorevaoy@users.noreply.github.com>
2019-10-25 08:55:22 +0300

    промежуточные работы с addressbook

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactToGroupAdditionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactToGroupAdditionTests.java
@@ -6,6 +6,11 @@ import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -13,6 +18,10 @@ public class ContactToGroupAdditionTests  extends TestBase{
 
   @BeforeMethod
   public void ensurePreconditions(){
+
+    ContactData a1 = app.db().contacts().iterator().next();
+   // Contacts contactsOutOfgroups = listOfContactsOutOfGroups(app.db().contacts());
+
     if(app.db().contacts().size() ==0){
       app.goTo().ContactPage();
       app.contact().create(new ContactData().withFirstname("Oksana").withLastname("Grigoreva").withAddress("Saint-Petersburg").withHomePhone("89112999959").withEmail("kiyrina@mail.ru"), true);
@@ -25,6 +34,14 @@ public class ContactToGroupAdditionTests  extends TestBase{
     }
   }
 
+  /* private Contacts listOfContactsOutOfGroups(Contacts contacts) {
+    for (contact: contacts){
+      contacts.iterator().next().getGroups();
+    }
+
+    return contacts;
+  } */
+
   @Test
   public void TestContactToGroupAddition(){
     Groups groups = app.db().groups();
@@ -34,13 +51,16 @@ public class ContactToGroupAdditionTests  extends TestBase{
 
     //проверка списка групп контакта до добавления в группу
     assertThat(addedContact.getGroups(), equalTo(app.db().contacts().iterator().next().getGroups()));
+   Groups groupsBeforeAddition = addedContact.getGroups();
 
     app.goTo().ContactPage();
     app.contact().addToGroup(addedContact,group);
     app.goTo().SelectedPage(group);
 
+   // assertThat(groupsBeforeAddition, equalTo(app.db().contacts().stream().map((g) -> new ContactData().getId(g.get).collect(Collectors.toSet())));
     //проверка списка групп контакта после добавления в группу
-    assertThat(addedContact.getGroups(), equalTo(app.db().contacts().iterator().next().getGroups()));
+    Groups groupsAfterAddition = app.db().contacts().iterator().next().getGroups();
+    assertThat(groupsBeforeAddition, equalTo(app.db().contacts().iterator().next().getGroups()));
 
 
   }
