3558e3a4a8cd184a2d83cac0a9a8fe2be15cbd74
grigorevaoy <55024506+grigorevaoy@users.noreply.github.com>
2019-10-29 15:32:07 +0300

    доработано 16 задание для теста удаления контакта из группы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactFromGroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactFromGroupDeletionTests.java
@@ -30,8 +30,23 @@ public class ContactFromGroupDeletionTests extends TestBase{
   @Test
   public void TestContactFromGroupDeletion(){
 
-    Contacts before = app.db().contacts();
+    Contacts contacts = app.db().contacts();
     Groups groups = app.db().groups();
+
+    GroupData group = app.db().getGroupFromDb(returnContactForDeletion(contacts,groups).getId()); //возвращаем группу, в которой есть контакты
+
+    app.contact().selectGroupForDeletion(group);
+    Contacts contactsForDeletion = group.getContacts();
+
+    ContactData removeContact = contactsForDeletion.iterator().next();
+    app.contact().deleteSelectedContactFromGroup(removeContact,group);
+    app.goTo().SelectedPage(group);
+
+    Contacts afterDeletionContacts = app.db().getGroupFromDb(group.getId()).getContacts();
+    assertThat(afterDeletionContacts, equalTo(contactsForDeletion.without(removeContact)));
+
+    //старый тест
+    /*
     GroupData group = groups.iterator().next();
     app.contact().selectGroupForDeletion(group);
     Contacts uiContacts = app.contact().all();
@@ -53,6 +68,24 @@ public class ContactFromGroupDeletionTests extends TestBase{
     Contacts afterDeletionContacts = app.db().getGroupFromDb(group.getId()).getContacts();
     assertThat(afterDeletionContacts, equalTo(beforeDeletionContacts.without(addedContact)));
 
+
+     */
+
   }
 
+  private GroupData returnContactForDeletion(Contacts contacts, Groups groups) {
+    for (ContactData contact : contacts) { //идем по всем контактам
+      if (contact.getGroups().size() > 0) { //смотрим, входят ли они в какие группы
+        Groups groupsWithContacts =  contact.getGroups();//если входят, берем список таких групп
+        return groupsWithContacts.iterator().next(); // возвращаем из списка следующую групп
+      }
+    } //если нет контактов в группах
+      ContactData addedContact = contacts.iterator().next(); //берем следующий контакт
+      GroupData group = groups.iterator().next(); //берем следующую группу
+      app.goTo().ContactPage();
+      app.contact().addToGroup(addedContact, group); //добавляем контакт в выбранную группу
+      app.goTo().SelectedPage(group);
+      return group; //возвращаем группу, в которую ранее добавили контакт
+
+  }
 }
