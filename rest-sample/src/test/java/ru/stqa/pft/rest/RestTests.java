daba243c92a941c0b79a42540c022d3e89db6732
grigorevaoy <55024506+grigorevaoy@users.noreply.github.com>
2019-11-03 20:22:32 +0300

    лекция 9.4

--- rest-sample/src/test/java/ru/stqa/pft/rest/RestTests.java
+++ rest-sample/src/test/java/ru/stqa/pft/rest/RestTests.java
@@ -4,12 +4,15 @@ import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import com.google.gson.reflect.TypeToken;
+import org.apache.http.client.fluent.Executor;
 import org.apache.http.client.fluent.Request;
 import org.apache.http.message.BasicNameValuePair;
 import org.testng.annotations.Test;
 
 
+import java.io.IOException;
 import java.util.Set;
+
 
 
 import static org.testng.Assert.assertEquals;
@@ -17,7 +20,7 @@ import static org.testng.Assert.assertEquals;
 public class RestTests {
 
   @Test
-  public void testCreateIssue(){
+  public void testCreateIssue() throws IOException {
     Set<Issue> oldIssues = getIssues();
     Issue newIssue = new Issue().withSubject("Test issue").withDescription("New test issue");
     int issueId = createIssue(newIssue);
@@ -27,7 +30,7 @@ public class RestTests {
 
   }
 
-  private Set<Issue> getIssues(){
+  private Set<Issue> getIssues() throws IOException {
     String json = getExecutor().execute(Request.Get("http://demo.bugify.com/api/issues.json"))
             .returnContent().asString();
     JsonElement parsed = new JsonParser().parse(json);
@@ -35,7 +38,11 @@ public class RestTests {
     return new Gson().fromJson(issues, new TypeToken<Set<Issue>>(){}.getType());
   }
 
-  private int createIssue(Issue newIssue) {
+  private Executor getExecutor() {
+    return Executor.newInstance().auth("LSGjeU4yP1X493ud1hNniA==", "");
+  }
+
+  private int createIssue(Issue newIssue) throws IOException {
     String json = getExecutor().execute(Request.Post("http://demo.bugify.com/api/issues.json")
             .bodyForm(new BasicNameValuePair("subject", newIssue.getSubject()),
                     new BasicNameValuePair("description", newIssue.getDescription())))
